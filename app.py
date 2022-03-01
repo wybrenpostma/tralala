2d7b791cbecae8d0b117e5f31bf7f18fdbba6ae6
Wybren Postma <wybren.postma@diasemi.com>
2022-03-01 14:27:19 +0100

    lala

---
+++ app.py
@@ -0,0 +1,20 @@
+from webbrowser import get
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+@app.route("/2")
+def hello_world2():
+    a = 1
+    b = 2
+    return f'hello, addition is {a+b}</p>'
+
+@app.route("/twee/<getal>")
+def hello_world3(getal):
+    var1 = int(getal)
+    var2 = var1 + var1
+    return f'Dit is een hele andere {str(var2)} functie</p>'
